1bd7ee195f32820003355507309899728a917dc1
Sdmitriss <dmitriss33@gmail.com>
2025-05-12 12:27:16 +0300

    fix: user_name split bug, added line 59

--- main_topics/src/module/dict_to_dataframe_parser.py
+++ main_topics/src/module/dict_to_dataframe_parser.py
@@ -56,7 +56,7 @@ class DictToDataFrameParser:
         if 'user_name_actor' in df.columns:
             df['user_name'] = df['user_name'].fillna(df['user_name_actor'])
             del df['user_name_actor']
-
+        df['user_name'] = df['user_name'].fillna('')
         df['chat_name'] =dict_to_df.get('name')
         df['chat_id'] =dict_to_df.get('id')
         column =list(df.columns)
